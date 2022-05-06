521c95d2596696428d41c662f98b4d4a2d5bd5f9
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-06 16:16:07 +0800

    update .gitignore

--- deps.ts
+++ deps.ts
@@ -6,9 +6,3 @@ export {
     shallowReadonly,
     stop,
 } from "https://esm.sh/@vue/reactivity@3.2.33/dist/reactivity.esm-bundler.js";
-
-// export {
-//     assert,
-//     assertEquals,
-// } from "https://deno.land/std@0.136.0/testing/asserts.ts";
-// export type { Ref } from "https://esm.sh/@vue/reactivity@3.2.33?dts";
